9da6cc4d4110af58b2ea7ad2f3ebc03f7ea9f093
MariaClaraLucas <148992514+MariaClaraLucas@users.noreply.github.com>
2024-05-27 08:48:03 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+numero = float(input('Digite o primeiro número:'))
+numero2 = float(input('Digite o segndo número:'))
+
+sum = numero + numero2
+print('a soma é:', sum)
+menus = numero - numero2
+print('a subtração é:', menus)
+mult = numero * numero2
+print('a subtração é:', mult)
+div = numero / numero2
+print('a divisão é:', div)
